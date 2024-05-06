36650177e8c3a0e354562e348becb63d78f5ed5a
Pedro <pedrosouza.senai564@gmail.com>
2024-05-06 11:00:21 -0300

    feat: Adicionar rota de batalha entre heróis

--- db/script.sql
+++ db/script.sql
@@ -19,4 +19,33 @@ FOREIGN KEY (winner_id) REFERENCES herois(id)
 );
 
 
-INSERT INTO herois (name, power, level, hp) VALUES ('', '', '', '');
+INSERT INTO herois (name, power, level, hp) VALUES
+('Superman', 'Super Força', 10, 100),
+('Batman', 'Inteligência', 8, 80),
+('Mulher-Maravilha', 'Super Força, Voo', 9, 90),
+('Flash', 'Super Velocidade', 9, 85),
+('Aquaman', 'Comunicação com animais marinhos', 7, 75),
+('Lanterna Verde', 'Anel do Poder', 8, 85),
+('Ciborgue', 'Tecnopatia', 7, 70),
+('Arqueiro Verde', 'Arquearia, Habilidades de Combate', 8, 85),
+('Shazam', 'Super Força, Velocidade, Invulnerabilidade', 9, 95),
+('Coringa', 'Mente Criminosa, Habilidades de Combate', 8, 80),
+('Mulher-Gato', 'Agilidade, Habilidades Acrobáticas', 7, 75),
+('Asa Noturna', 'Habilidades de Combate, Acrobacias', 8, 85),
+('Ravena', 'Magia, Manipulação de Emoções', 9, 90),
+('Cyborgirl', 'Tecnopatia, Resistência Cibernética', 8, 85),
+('Batgirl', 'Inteligência, Habilidades de Combate', 8, 85),
+('Homem de Ferro', 'Tecnologia Avançada', 9, 95),
+('Capitão América', 'Super Soldado', 8, 90),
+('Thor', 'Deus do Trovão', 10, 100),
+('Hulk', 'Super Força', 9, 95),
+('Viúva Negra', 'Espionagem', 8, 85),
+('Gavião Arqueiro', 'Arquearia', 7, 80),
+('Feiticeira Escarlate', 'Manipulação da Realidade', 9, 90),
+('Visão', 'Inteligência Artificial, Força Sobre-Humana', 9, 95),
+('Máquina de Combate', 'Armadura de Combate', 8, 85),
+('Falcão', 'Voo, Combate Aéreo', 7, 80),
+('Homem-Aranha', 'Agilidade, Sentido-Aranha', 8, 85),
+('Pantera Negra', 'Força, Agilidade, Tecnologia Avançada', 9, 95),
+('Doutor Estranho', 'Magia, Manipulação Dimensional', 10, 100),
+('Capitã Marvel', 'Super Força, Voo, Resistência', 10, 100);
--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/herois', async (req, res) => {
     }
 });
 
+//Criar um heroi
 app.post('/herois', async (req, res) => {
     try {
         const { name, power, level, hp } = req.body;
@@ -45,6 +46,7 @@ app.post('/herois', async (req, res) => {
     }
 });
 
+//Atualizar um heroi
 app.put('/herois/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -59,6 +61,7 @@ app.put('/herois/:id', async (req, res) => {
     }
 });
 
+//Deletar um heroi
 app.delete('/herois/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -70,6 +73,7 @@ app.delete('/herois/:id', async (req, res) => {
     }
 });
 
+//Pegar um heroi pelo id dele
 app.get('/herois/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -84,6 +88,40 @@ app.get('/herois/:id', async (req, res) => {
     } catch (error) {
         console.error('Erro ao obter o heroi', error);
         res.status(500).send('Erro ao obter o heroi');
+    }
+});
+
+
+app.get('/batalha/:id1/:id2', async (req, res) => {
+    try {
+        const { id1, id2 } = req.params;
+        const resultado1 = await pool.query('SELECT * FROM herois WHERE id = $1', [id1]);
+        const resultado2 = await pool.query('SELECT * FROM herois WHERE id = $1', [id2]);
+
+        if (resultado1.rowCount === 0 || resultado2.rowCount === 0) {
+            return res.status(404).send('Um ou ambos os heróis não foram encontrados');
+        }
+
+        const heroi1 = resultado1.rows[0];
+        const heroi2 = resultado2.rows[0];
+
+        let vencedor;
+        if ((heroi1.level && heroi2.level && heroi1.hp && heroi2.hp) &&
+            (heroi1.level > heroi2.level || (heroi1.level === heroi2.level && heroi1.hp > heroi2.hp))) {
+            vencedor = heroi1;
+        } else {
+            vencedor = heroi2;
+        }
+
+        // Salvar o resultado da batalha no histórico
+        await pool.query('INSERT INTO batalhas (heroes1_id, heroes2_id, winner_id) VALUES ($1, $2, $3)', [id1, id2, vencedor.id]);
+
+        res.json({
+            vencedor,
+        });
+    } catch (error) {
+        console.error('Erro ao realizar a batalha', error);
+        res.status(500).send('Erro ao realizar a batalha');
     }
 });
 
